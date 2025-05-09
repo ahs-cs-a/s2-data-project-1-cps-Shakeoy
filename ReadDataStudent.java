6bb2388592e320a3d0e032722bbad9d7f74f9272
Shakeoy <146458152+Shakeoy@users.noreply.github.com>
2025-05-09 17:49:57 +0000

    123123

--- ReadDataStudent.java
+++ ReadDataStudent.java
@@ -1,140 +1,124 @@
 import java.util.Scanner;
 import java.io.File;
 
-
-public class ReadDataStudent{
-    //I hard-coded the number of rows and columns so 
-    //I could use a 2D array
+public class ReadData {
     private double[][] data = new double[21908][14];
 
-    //This should read in the csv file and store the data in a 2D array,
-    //data -- don't forget to skip the header line and parse everything
-    //as doubles  
-    public void read(){
-        try{
+    
+    public void read() {
+        try {
             Scanner scanner = new Scanner(new File("cps.csv"));
             int row = 0;
             scanner.nextLine(); // Skip the header line
-            while(scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] lineArr = line.split(",");
-                // for(int i=0; i <data[row].length;i++){
-                //    double a=scanner.nextDouble();
-                //     data[row][i] = a;
-                // }
-                for(int i=0; i<lineArr.length;i++){
+                for (int i = 0; i < lineArr.length; i++) {
                     data[row][i] = Double.parseDouble(lineArr[i]);
                 }
                 row++;
             }
             scanner.close();
-    
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //this should return the column of data based
-    //on the column number passed in -- the column number
-    //is 0 indexed, so the first column is 0, the second
-    //is 1, etc.
-    //this should return a double array of the column
-    //of data
-    public double[] getColumn(int col){
-        double[] column = new double[data.length];
-        for(int i=0;i<data.length;i++){
-            column[i] = data[i][col];
+    
+    public double[][] getColumns(int col1, int col2) {
+        double[][] column = new double[data.length][2];
+        for (int i = 0; i < data.length; i++) {
+            column[i][0] = data[i][col1];
+            column[i][1] = data[i][col2];
         }
         return column;
     }
 
-    //this returns the standard deviation of the column
-    //of data passed in
-    //the standard deviation is the square root of the variance
-    //the variance is the sum of the squares of the differences between each value and the mean, 
-    //divided by the number of values - 1(sample variance)
-    //Use Math.pow to square the difference
-    //and Math.sqrt to take the square root
-    public double stdDeviation(double[] arr){
-        double sum = 0;
-        double mean = 0;
-        for(int i=0;i<arr.length;i++){
-             sum += arr[i];
+    
+    public double[] stdDeviation(double[][] xy) {
+        double sumx = 0, sumy = 0;
+        for (int i = 0; i < xy.length; i++) {
+            sumx += xy[i][0];
+            sumy += xy[i][1];
         }
-        mean = sum/arr.length;
-        double a = 0;
-        for (double num : arr) {
-            a += Math.pow(num - mean, 2);
+        double meanX = sumx / xy.length;
+        double meanY = sumy / xy.length;
+
+        double sumSqX = 0, sumSqY = 0;
+        for (int i = 0; i < xy.length; i++) {
+            sumSqX += Math.pow(xy[i][0] - meanX, 2);
+            sumSqY += Math.pow(xy[i][1] - meanY, 2);
         }
-        double b = a / (arr.length - 1);
-        return Math.sqrt(b);
-    }
-   
-    //this returns the mean of the column of data passed in
-    //the mean is the sum of the values divided by the number of values
-    public double mean(double[] arr){
-        double sum = 0;
-        for(int i=0;i<arr.length;i++){
-            sum += arr[i];
-       }
-        return sum/arr.length;
+
+        
+        double stdDevX = Math.sqrt(sumSqX / (xy.length - 1));
+        double stdDevY = Math.sqrt(sumSqY / (xy.length - 1));
+
+        return new double[]{stdDevX, stdDevY};
     }
 
-    //this returns the values of a column in standard units
-    //the standard units are the value minus the mean divided by the standard deviation
-    //this should return a double array of the standard units
-    public double[] standardUnits(double[] arr){
-        double[] stdArr = new double[arr.length];
-        double stdDeviation = stdDeviation(arr);
-        double mean = mean(arr);
-        for(int i=0; i <arr.length; i++){
-            stdArr[i] = (arr[i]-mean)/stdDeviation;
+   
+    public double[] mean(double[][] xy) {
+        double sumX = 0, sumY = 0;
+        for (int i = 0; i < xy.length; i++) {
+            sumX += xy[i][0];
+            sumY += xy[i][1];
+        }
+        return new double[]{sumX / xy.length, sumY / xy.length};
+    }
+
+    
+    public double[][] standardUnits(double[][] xy) {
+        double[][] stdArr = new double[xy.length][2];
+        double[] stdDevs = stdDeviation(xy);
+        double[] means = mean(xy);
+
+        for (int i = 0; i < xy.length; i++) {
+            stdArr[i][0] = (xy[i][0] - means[0]) / stdDevs[0]; 
+            stdArr[i][1] = (xy[i][1] - means[1]) / stdDevs[1];  
         }
         return stdArr;
     }
-    
-    //this returns the correlation between the two columns of data passed in
-    //the correlation is the sum of the products of the standard units
-    //of the two columns divided by the number of values - 1
-    //this should return a double
-    //the correlation is a measure of the strength of the linear relationship
-    //between the two columns of data
-    //the correlation is between -1 and 1
-    public double correlation(double[] x, double[] y){
-            double sum=0;
-            for( int i=0; i< (x.length) || i< (y.length);i++){
-                sum+=x[i]*y[i];
-            }
-               
-            return sum/(x.length-1);
+
+  
+    public double correlation(double[][] xy) {   
+        double[][] stdUnits = standardUnits(xy);
+        double sum = 0;
+        for (int i = 0; i < xy.length; i++) {
+            sum += stdUnits[i][0] * stdUnits[i][1];
+        }
+        return sum / (xy.length - 1);
     }
-    
-    public void runRegression(){
-        double[] x = getColumn(7);
-        double[] y = getColumn(9);
-        double[] xStd = standardUnits(x);
-        double[] yStd = standardUnits(y);
-        double correlation = correlation(xStd, yStd);
-        double slope = correlation * stdDeviation(y) / stdDeviation(x);
-        double intercept = mean(y) - slope * mean(x);
+
+   
+    public void runRegression() {
+        double[][] xy = getColumns(7, 9); 
+        double[][] xyStd = standardUnits(xy); 
+        double correlation = correlation(xyStd);
+        double[] stdDevs = stdDeviation(xy); 
+        double slope = correlation * (stdDevs[1] / stdDevs[0]); 
+        double[] means = mean(xy); 
+        double intercept = means[1] - slope * means[0]; 
+
+        // Print the results
         System.out.println("Correlation: " + correlation);
         System.out.println("Slope: " + slope);
         System.out.println("Intercept: " + intercept);
-        Scatter s = new Scatter();
-        s.displayScatterPlot(x, y);
     }
 
-    //this prints the array passed in - you may want this for debugging
-    public void print(double[] arr){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+    // Print the array (for debugging purposes)
+    public void print(double[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        ReadDataStudent rd = new ReadDataStudent();
-        rd.read();
-        rd.runRegression();
     }
 
+    public static void main(String[] args) {
+        ReadData rd = new ReadData();
+        rd.read(); // Read the data from the CSV file
+        rd.runRegression(); // Run the regression on the 7th and 9th columns
+    }
 }
